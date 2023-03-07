3e61ed292147af3c14cd7a605f6bd850bd379369
Klyment <klim86kr@gmail.com>
2023-03-08 00:39:54 +0200

    01js

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+
+const navEl = document.querySelector('ul');
+const navEli = document.querySelector('li');
+const navAllEliments = document.querySelectorAll('ul');
+
+console.log(`Number of categories: ${navAllEliments}`);
+console.log(`Category: Animal`);
+console.log(`Elements: `);
+console.log(`Category: Product`);
+console.log(`Elements: `);
+console.log(`Category: Technologies`);
+console.log(`Elements: `);
+
+// navAllEliments.forEach((item) => console.log(item));
+// console.log(navEl);
+// console.log(navEli);
