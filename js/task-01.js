49e77a55db5ad9960f7cf86751c9472c3ca1223f
Klyment <klim86kr@gmail.com>
2023-03-08 00:46:19 +0200

    fix1

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,10 @@
 
 const navEl = document.querySelector('ul');
 const navEli = document.querySelector('li');
-const navAllEliments = document.querySelectorAll('ul');
+const navAllEliments = document.querySelectorAll('.ul-list');
+console.log(`Number of categories:`, navAllEliments);
 
-console.log(`Number of categories: ${navAllEliments}`);
+// console.log(`Number of categories: ${navAllEliments}`);
 console.log(`Category: Animal`);
 console.log(`Elements: `);
 console.log(`Category: Product`);
@@ -11,6 +12,6 @@ console.log(`Elements: `);
 console.log(`Category: Technologies`);
 console.log(`Elements: `);
 
-// navAllEliments.forEach((item) => console.log(item));
+ navAllEliments.forEach((item) => console.log(item));
 // console.log(navEl);
 // console.log(navEli);
