495154b92e909aaeeaf354fd8ad1897e6e0e9576
Klyment <klim86kr@gmail.com>
2023-03-12 13:36:10 +0200

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,48 +1,22 @@
-// const htmlElem = document.documentElement;
-// console.log(htmlElem);
-// const head = document.head;
-// console.log(head);
-// const body = document.body;
-// console.log(body);
-// const list = (body).children;
-// console.log(list);
-
-
 //Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-const navAllEliments = document.querySelectorAll('.item').length; 
-console.log(`Number of categories:`, navAllEliments);
+const navTitleElments = document.querySelectorAll('.item').length; 
+console.log(`Number of categories:`, navTitleElments);
 
 // Для кожного элемента li.item у списку ul#categories
-const listItemElements = document.querySelector('h2');
-console.log(`Category:`, listItemElements);
+const itemElements = document.querySelectorAll('li.item');
 
-const navElCategories = document.querySelectorAll('ul').children.length;
-// const navEli = document.querySelector('li');
-// const navAllEliments = document.querySelectorAll('ul');
+itemElements.forEach(item => {
+    // текст заголовку елемента (тегу <h2>)
+    const titleName = item.querySelector('h2').textContent;
+    console.log(`Category:`, titleName);
 
-let countCategories = 0;
-navElCategories.forEach((item) => item+=countCategories); 
-console.log(countCategories);
+    // кількість елементів в категорії (усіх <li>, вкладених в нього).
+    const categoryElements = document.querySelectorAll('ul li').length;
+    console.log(`Elements:`, categoryElements);
 
-console.log(`Number of categories: ${countCategories}`);
-console.log(`Category: Animal`);
-console.log(`Elements: `);
-console.log(`Category: Product`);
-console.log(`Elements: `);
-console.log(`Category: Technologies`);
-console.log(`Elements: `);
+});
 
 
 
 
-// console.log(`Number of categories: ${navAllEliments}`);
-// console.log(`Category: Animal`);
-// console.log(`Elements: `);
-// console.log(`Category: Product`);
-// console.log(`Elements: `);
-// console.log(`Category: Technologies`);
-// console.log(`Elements: `);
 
-//  navAllEliments.forEach((item) => console.log(item));
-// console.log(navEl);
-// console.log(navEli);
