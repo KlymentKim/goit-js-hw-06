82c9fa27a84bbc64379eb20d891013150fbe8ec1
Klyment <klim86kr@gmail.com>
2023-03-12 12:54:17 +0200

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,31 @@
+const htmlElem = document.documentElement;
+console.log(htmlElem);
+// const navEl = document.querySelector('ul');
+// const navEli = document.querySelector('li');
+const head = document.head;
+console.log(head);
+const body = document.body;
+console.log(body);
+// const list = (body).children;
+// console.log(list);
 
-const navEl = document.querySelector('ul');
-const navEli = document.querySelector('li');
-const navAllEliments = document.querySelectorAll('.ul-list');
+//Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+const navAllEliments = document.querySelectorAll('.item').length; 
 console.log(`Number of categories:`, navAllEliments);
 
+// Для кожного элемента li.item у списку ul#categories
+const listItemElements = document.querySelector('h2');
+console.log(`Category:`, listItemElements);
+
+
+
 // console.log(`Number of categories: ${navAllEliments}`);
-console.log(`Category: Animal`);
-console.log(`Elements: `);
-console.log(`Category: Product`);
-console.log(`Elements: `);
-console.log(`Category: Technologies`);
-console.log(`Elements: `);
+// console.log(`Category: Animal`);
+// console.log(`Elements: `);
+// console.log(`Category: Product`);
+// console.log(`Elements: `);
+// console.log(`Category: Technologies`);
+// console.log(`Elements: `);
 
 //  navAllEliments.forEach((item) => console.log(item));
 // console.log(navEl);
