159fef125e53958369fe69d73aee3a0d1b8ba6fb
Klyment <49413026+klim86klim@users.noreply.github.com>
2023-03-13 17:06:07 +0200

    fix

--- js/task-04.js
+++ js/task-04.js
@@ -2,10 +2,10 @@
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
 
-
 const dicrementButtonClickListener = document.querySelector('[data-action="decrement"]');
 const incrementButtonClickListener = document.querySelector('[data-action="increment"]');
-const counterValueElement = document.getElementById('#value');
+const counterValueElement = document.getElementById('value');
+// const counterValueElement = document.querySelector('#value');
 
 let counterValue = 0;
 dicrementButtonClickListener.addEventListener('click', () => { 
