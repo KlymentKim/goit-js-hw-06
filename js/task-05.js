e217a73868d7ca982dcfc0b009e6c6416a65321d
Klyment <49413026+klim86klim@users.noreply.github.com>
2023-03-15 10:56:33 +0200

    fix

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name - input(подія input),
+// підставляє його поточне значення в span#name - output.
+// Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+// const changeValue = () => {
+//     return document.getElementById('name-input').value = 'Anonymous';
+// }
+const nameInPut = document.getElementById('name-input');
+const nameOutPut = document.getElementById('name-output');
+
+nameInPut.addEventListener('input', () => {
+ nameOutPut.textContent = nameInPut.value || 'Anonymous'
+});
