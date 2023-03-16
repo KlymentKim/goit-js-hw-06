30a1d0af6e5b2d5b354306be6403a5fc0db63193
Klyment <49413026+klim86klim@users.noreply.github.com>
2023-03-16 16:08:57 +0200

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,22 +1,27 @@
+// Напиши скрипт, який:
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента 
+// (тегу < h2 >) і кількість елементів в категорії(усіх < li >, вкладених в нього).
+// Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
+
+
 //Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
 const navTitleElments = document.querySelectorAll('.item').length; 
 console.log(`Number of categories:`, navTitleElments);
 
-// Для кожного элемента li.item у списку ul#categories
-const itemElements = document.querySelectorAll('li.item');
+// покук Для кожного элемента li.item у списку ul#categories
+const itemElements = document.querySelectorAll('.item');
 
 itemElements.forEach(item => {
-    // текст заголовку елемента (тегу <h2>)
+    // витягуємо текст заголовку елемента (тегу <h2>)
     const titleName = item.querySelector('h2').textContent;
     console.log(`Category:`, titleName);
 
     // кількість елементів в категорії (усіх <li>, вкладених в нього).
-    const categoryElements = document.querySelectorAll('.item').length;
-    console.log(`Elements:`, categoryElements);
+    const sumLiItems = item.lastElementChild.children.length;
+    console.log(`Elements:`, sumLiItems);
 
 });
-
-
 
 
 
